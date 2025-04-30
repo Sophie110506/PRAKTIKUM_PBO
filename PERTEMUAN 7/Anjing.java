9fdae939a15722c131e22566d224730f6f1d44b4
Sophie Venecia May Manalu <159929966+Sophie110506@users.noreply.github.com>
2025-04-30 15:40:55 +0700

    Anjing.java

--- PERTEMUAN 7/Anjing.java
+++ PERTEMUAN 7/Anjing.java
@@ -11,11 +11,11 @@ public class Anjing extends Anabul {
 
     @Override
     public void Gerak() {
-        System.out.println(nama + " Gerak : Melata ");
+        System.out.println(nama + " Bergerak Melata ");
     }
 
     @Override
     public void Bersuara() {
-        System.out.println(nama + " Suara : Guk-guk");
+        System.out.println(nama + " Bersuara Guk-guk");
     }
 }
